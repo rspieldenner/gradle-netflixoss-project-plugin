0f8274955e339deb9cffd100ddd91c0aea1c2e05
Rob Spieldenner <rob.spieldenner@gmail.com>
2014-09-15 16:46:32 -0700

    Add in plugins for a simple flat project

--- src/test/groovy/nebula/plugins/netflixossproject/NetflixOssProjectPluginSpec.groovy
+++ src/test/groovy/nebula/plugins/netflixossproject/NetflixOssProjectPluginSpec.groovy
@@ -1,15 +1,54 @@
 package nebula.plugins.netflixossproject
 
+import nebula.plugin.contacts.ContactsPlugin
+import nebula.plugin.dependencylock.DependencyLockPlugin
+import nebula.plugin.info.InfoPlugin
+import nebula.plugin.publishing.NebulaJavadocJarPlugin
+import nebula.plugin.publishing.NebulaPublishingPlugin
+import nebula.plugin.publishing.NebulaSourceJarPlugin
 import nebula.test.PluginProjectSpec
+import org.gradle.api.JavaVersion
+import org.gradle.api.plugins.JavaPlugin
 
 class NetflixOssProjectPluginSpec extends PluginProjectSpec {
-    String pluginName = 'netflixOssProject'
+    String pluginName = 'netflixoss-project'
 
-    def 'plugin adds other plugins successfully'() {
+    def 'plugin adds other plugins successfully to a simple project'() {
         when:
-        def x = 1 + 1
+        project.plugins.apply(NetflixOssProjectPlugin)
 
         then:
-        x == 2
+        project.plugins.findPlugin(NebulaPublishingPlugin) != null
+        project.plugins.findPlugin(NebulaJavadocJarPlugin) != null
+        project.plugins.findPlugin(NebulaSourceJarPlugin) != null
+        project.plugins.findPlugin(InfoPlugin) != null
+        project.plugins.findPlugin(ContactsPlugin) != null
+        project.plugins.findPlugin(DependencyLockPlugin) != null
     }
+
+    def 'plugin sets default java version'() {
+        project.plugins.apply(NetflixOssProjectPlugin)
+
+        when:
+        project.plugins.apply(JavaPlugin)
+
+        then:
+        project.sourceCompatibility == JavaVersion.VERSION_1_6
+        project.targetCompatibility == JavaVersion.VERSION_1_6
+    }
+
+    def 'plugin allows changing java version'() {
+        project.plugins.apply(NetflixOssProjectPlugin)
+        project.plugins.apply(JavaPlugin)
+
+        when:
+        project.sourceCompatibility = '1.7'
+        project.targetCompatibility = '1.7'
+
+        then:
+        project.sourceCompatibility == JavaVersion.VERSION_1_7
+        project.targetCompatibility == JavaVersion.VERSION_1_7
+    }
+
+
 }
